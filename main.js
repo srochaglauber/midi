be7928f122f8a6292059fd135d2ab6fc4645dc38
srochaglauber <srochaglauber@gmail.com>
2022-06-09 15:23:27 -0300

    Clique no botão

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ function tocaSomClap() {
     document.querySelector('#som_tecla_clap').play();
 }
 
-document.querySelector('.tecla_pom');
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
